de01ae115fd7fd8ba670c94d4a6843dbce2153a0
andrei1714 <103685128+andrei1714@users.noreply.github.com>
2024-03-19 14:53:58 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { VitePluginGhPages } from 'vite-plugin-gh-pages';
+import pkg from 'vite-plugin-gh-pages';
+const VitePluginGhPages = pkg.default;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePluginGhPages()],
+  plugins: [react(), VitePluginGhPages],
 
 })
